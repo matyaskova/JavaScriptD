62cc434a0c6e0507a885718ff157cc1f9eff65e1
matyaskova <43047469+matyaskova@users.noreply.github.com>
2024-09-25 11:43:39 +0200

    nuly

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ let mesic=datum.getMonth()+1
 let rok =datum.getFullYear()
 
 document.getElementById("datum").innerText="Dnes je "+den+". "+mesic+". "+rok
+
+function cas(){
 let hodina = datum.getHours()
 let minuta = datum.getMinutes()
 let sekunda = datum.getSeconds()
+
+if (minuta<10)
+	minuta = "0"+minuta
+if (sekunda<10)
+	sekunda = "0"+sekunda
 document.getElementById("cas").innerText=hodina+":"+minuta+":"+sekunda
+}
+cas()
